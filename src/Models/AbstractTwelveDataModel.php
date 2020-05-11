a9916c8ca3d9a310388d9210705a37eae6f78f02
George Hallam <george@ingelby.com>
2020-05-11 16:44:15 +0000

    - Added formatter to base model

--- src/Models/AbstractTwelveDataModel.php
+++ src/Models/AbstractTwelveDataModel.php
@@ -8,4 +8,13 @@ use yii\base\Model;
 
 abstract class AbstractTwelveDataModel extends Model
 {
+    /**
+     * @param float $value
+     * @param int   $decimalPlaces
+     * @return string
+     */
+    public function getInFormat($value, int $decimalPlaces = 2): string
+    {
+        return number_format((float)$value, $decimalPlaces);
+    }
 }
